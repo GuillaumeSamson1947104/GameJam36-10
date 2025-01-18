178461141d9c63db3599444421d13ae7cfcb3c77
Loic <Lohco007@gmail.com>
2025-01-18 14:22:39 -0500

    new movement

--- gj/Scenes/Player/node_2d.gd
+++ gj/Scenes/Player/node_2d.gd
@@ -1,45 +1,67 @@
 extends CharacterBody2D
 @export var jumpspeed : float = -600
 @export var speed : float = 400
-const  GRAVITY = 900
-var start_timer
-
-func _physics_process(delta):
-	var direction = Input.get_axis("move_left","move_right")
-	
-	if direction:
-		velocity.x = direction * speed
-		
-	else:
-		velocity.x = 0
-		
+const  GRAVITY = 3000
+var start_timer 
+var total_time
+var player_direction
+func _process(delta):
+	inputs()
+	print(player_direction)
+	calculate_jump()
+	#print(rotation_degrees)
 	if !is_on_floor():
 		velocity.y += GRAVITY * delta
 		
-	if Input.is_action_just_pressed("jump"):
-		if is_on_floor():
-			velocity.y = jumpspeed
-			
-		else:
-			pass
 	
 	move_and_slide()
 
-
 func calculate_jump():
-	if Input.is_action_just_pressed("jump"):
-		start_timer.get_ticks_msec()
+	if is_on_floor():
 		
-	if Input.is_action_just_released("jump"):
-		var end_timer
-		#end_time.get_ticks_msec()
-		var total_time = end_timer - start_timer
-		print(total_time)
+		if Input.is_action_just_pressed("jump"):
+			start_timer = Time.get_ticks_msec()
+		
+		if Input.is_action_just_released("jump"):
+			var end_timer = Time.get_ticks_msec()
+		
+			total_time = start_timer - end_timer
+			var jump_strength = clamp(total_time, -3000, -200)
+		
+			velocity.y = jump_strength
+		
+			if player_direction == 1:
+				velocity.x = jump_strength * -1
+			elif player_direction == -1:
+				velocity.x = jump_strength * 1
+			else:
+				velocity.x = 0
+		
+	
+	#var jump_strength = clamp(total_time, 200, 2000)
+	
+	#velocity = jump_strength
 
 func _unhandled_input(event):
-	inputs(event)	
+	pass
+	#inputs(event)
 	
+func inputs():
 	
-func inputs(event):
-	if Input.is_action_just_pressed("jump"):
-		print("jump")
+	var direction = Input.get_axis("move_left","move_right")
+	if direction != 0:
+		player_direction = direction
+	# MOVEMENT
+	if direction:
+		velocity.x = direction * speed
+		
+	elif is_on_floor():
+		velocity.x = 0
+		
+	# ROTATION
+	if direction == 1:
+		rotation_degrees = 90
+	if direction == -1:
+		rotation_degrees = -90
+
+	
