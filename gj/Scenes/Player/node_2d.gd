cbb66b746e62e96a4106bc7c15902b4991b07901
GuillaumeSamson1947104 <46759161+GuillaumeSamson1947104@users.noreply.github.com>
2025-01-18 16:01:16 -0500

    bounce commit

--- gj/Scenes/Player/node_2d.gd
+++ gj/Scenes/Player/node_2d.gd
@@ -5,16 +5,26 @@ const  GRAVITY = 3000
 var start_timer 
 var total_time
 var player_direction
+var collision_info
+var animationState = AnimationState.NEUTRAL
+
+enum AnimationState {
+	NEUTRAL,
+	BOUNCE
+} 
+
+
 func _process(delta):
 	inputs()
-	print(player_direction)
 	calculate_jump()
 	#print(rotation_degrees)
 	if !is_on_floor():
 		velocity.y += GRAVITY * delta
-		
 	
+	var tempVel = velocity
 	move_and_slide()
+	
+	calculateBounce(tempVel)
 
 func calculate_jump():
 	if is_on_floor():
@@ -42,6 +52,17 @@ func calculate_jump():
 	
 	#velocity = jump_strength
 
+func calculateBounce(tempVelocity : Vector2):
+	if get_slide_collision_count() > 0 && !is_on_floor():
+		print(tempVelocity)
+		var collision = get_slide_collision(0)
+		if collision != null:
+			velocity = tempVelocity.bounce(collision.get_normal())
+			velocity.x *= 0.8
+			player_direction *= -1
+			rotateSprite(player_direction)
+			#$Sprite2D.rotate(20*player_direction)
+
 func _unhandled_input(event):
 	pass
 	#inputs(event)
@@ -58,10 +79,10 @@ func inputs():
 	elif is_on_floor():
 		velocity.x = 0
 		
-	# ROTATION
+	rotateSprite(direction)	
+	
+func rotateSprite(direction):
 	if direction == 1:
 		$Sprite2D.flip_h = false
 	if direction == -1:
 		$Sprite2D.flip_h = true
-
-	
