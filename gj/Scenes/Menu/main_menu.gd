a46e4171e3fc6030d56e377889516863881551ad
GuillaumeSamson1947104 <46759161+GuillaumeSamson1947104@users.noreply.github.com>
2025-01-19 13:49:04 -0500

    Menu

--- gj/Scenes/Menu/main_menu.gd
+++ gj/Scenes/Menu/main_menu.gd
@@ -1,4 +1,11 @@
 extends Control
 
 func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/Level/main.tscn")
+	$MarginContainer/HBoxContainer/CenterContainer/Start.visible = false
+	$MarginContainer/HBoxContainer/VBoxContainer/Label.visible = false
+	$MarginContainer/MenuAnimationLoop.play("start")
+
+func _on_menu_animation_loop_animation_looped() -> void:
+	print($MarginContainer/MenuAnimationLoop.animation )
+	if $MarginContainer/MenuAnimationLoop.animation == "start" :
+		get_tree().change_scene_to_file("res://Scenes/Level/main.tscn")
